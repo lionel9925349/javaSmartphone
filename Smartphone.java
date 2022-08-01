44b954f7a390f00415ead156b3e680bed9c987d3
Ulrich Lionel Noumi Noumi <ulrich.lionel.noumi.noumi@mni.thm.de>
2022-08-01 16:56:01 +0200

    methodeFindKontaktUndAddKontaktFertig

--- Smartphone.java
+++ Smartphone.java
@@ -9,4 +9,28 @@ public class Smartphone {
     public Smartphone() {
         meineKontakte = new ArrayList<Kontakt>();
     }
+
+    public boolean findKontakt (Kontakt name){
+
+        for( Kontakt  KontaktInListe : meineKontakte){
+
+            if (name.getName().equals(KontaktInListe.getName()));
+            System.out.printf("Kontakt ist bereit da ");
+            return  false;
+        }
+        return  true;
+
+    }
+
+    public  boolean addKontakt (Kontakt neuerKontakt){
+        if (findKontakt(neuerKontakt)== true){
+            meineKontakte.add(neuerKontakt);
+        }
+        else
+        {
+            System.out.println("kontakt kann nicht hinzugefuegt");
+            return false;
+        }
+        return true;
+    }
 }
